8cfaa5002e5be4cd19fa8eb61ea66a321088b409
RyUmarof <ryumarof@gmail.com>
2024-11-01 18:45:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let myArray = ["anor", "nok", "uzum", "shaptoli"];
+
+
+//console.log("Array :", myArray);
+alert("Array : " + myArray);
+
+if (confirm("Arraydan bitta element qoshilishini xohlaysizmi?")) {
+
+         myArray.push('banan')
+         
+         
+} else {
+         console.log(myArray.pop());
+         
+}
+
+console.log("Yangilangan array:", myArray);
+alert("Yangilangan array: " + myArray);
